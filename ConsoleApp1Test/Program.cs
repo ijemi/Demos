3210ec1759c7a7625ac18ac3e446050b217d6ada
ijemi <1305019658@qq.com>
2019-05-07 23:21:10 +0800

    考勤功能

---
+++ ConsoleApp1Test/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1Test
+{
+    public class Program
+    {
+        static int a = 100;
+        static int b = 0;
+        static int c = 0;
+        static int d = 350;
+        static DateTime aa;
+        static DateTime bb;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                aa = DateTime.Now;
+
+                Random random = new Random();
+                var r = random.Next() % 4;
+                Thread.Sleep(r + 1000);
+
+                bb = DateTime.Now;
+
+                var ts = (bb - aa).TotalSeconds;
+                if (ts >= 3)
+                {
+                    ++a;
+                    ++b;
+                    if (a == 500)
+                    {
+                        a = 100;
+                        aa = bb;
+                    }
+                }
+                c = d / a * 100;
+
+                Console.WriteLine(string.Format("a={0},b={1},c={2}", a, b, c));
+            }
+        }
+
+    }
+}
